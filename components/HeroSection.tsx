017c4473ae24a03749fb87d4bfbddc6948d77509
satyam07july-png <satyam07july@gmail.com>
2026-09-26 10:39:25 +0530

    inital chnages

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -373,6 +373,7 @@ export default function HeroSection({ onLeadSuccess }: HeroSectionProps) {
                       <option value="College Student - 1st to 3rd Year">College Student (1st to 3rd Year)</option>
                       <option value="Fresher / Job Seeker">Fresher / Job Seeker</option>
                       <option value="Career Gap / Transition">Career Gap / Transition</option>
+                      <option value="others">others</option>
                     </select>
                   </div>
 
